12fe8109b188c1bb7c8211a209ece3d551c107ef
Joshua Abbott <joshnabbott@gmail.com>
2011-09-22 12:05:05 -0700

    Have a very troubled version of the console working

--- pandorabox_proxy.rb
+++ pandorabox_proxy.rb
@@ -4,9 +4,13 @@ require 'json'
 
 get '/' do
   content_type :json
+  headers \
+    "Access-Control-Allow-Origin" => "*"
 
   options = { :query => { :botid => params[:botid], :input => params[:message], :custid => params[:custid] } }
   response = HTTParty.post('http://www.pandorabots.com/pandora/talk-xml', options)
+  $stderr.puts "Response:"
+  $stderr.puts response.to_yaml
   response.parsed_response.to_json
 end
 
