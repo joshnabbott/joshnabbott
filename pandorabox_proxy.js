3e6b932916ffdc5d29d36be126e6781e63b39f61
Joshua Abbott <joshnabbott@gmail.com>
2011-09-23 10:01:06 -0700

    Set content-type

--- pandorabox_proxy.js
+++ pandorabox_proxy.js
@@ -2,9 +2,11 @@ var http = require('http');
 
 http.createServer(function (request, response) {
   response.writeHead(200, {
-    'Content-Type': 'text/plain',
+    'Content-Type': 'application/json',
     'Access-Control-Allow-Origin' : '*'
   });
+
+  console.log('Request\n');
 
   response.end('Hello World\n');
 }).listen(8124, '127.0.0.1');
