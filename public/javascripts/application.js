4e4c27492329fc87e869190c5a06e5191c18ca86
Joshua Abbott <joshnabbott@gmail.com>
2011-10-07 12:31:08 -0700

    Adding jquery iecors to CORS enable this for IE browsers

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
-  $('body').append('<script src="public/javascripts/console.js">\x3C/script>');
+  $('body').append(function() {
+    var script = '<script src="public/javascripts/console.js">\x3C/script>';
+    script += '<!--[if lt IE 10]>';
+    script += '<script src="public/javascripts/jquery.iecors.js">\x3C/script>';
+    script += '<![endif]-->';
+    return script;
+  });
 
   $('body').click(function() {
     if(Console.loaded) {
